87cf8a09ac5b19365097611a5c4d3b06bd7fd06a
UncleDoubleU <alexmozagba@gmail.com>
2026-02-19 11:54:01 +0100

    basic desktop footer section added

--- src/components/IssueInfo/IssueInfo.tsx
+++ src/components/IssueInfo/IssueInfo.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import sty from './IssueInfo.module.sass'
 
 function IssueInfo() {
      return (
-          <footer>
-               <h1>Void Outline</h1>
-               <p>issue N02 - Void Outline</p>
-               <p>june 2025</p>
+          <footer className={sty.footer}>
+               <h1 className={sty.h1}>Void Outline</h1>
+               <p className={sty.issue}>issue N02 - Void Outline</p>
+               <p className={sty.date}>june 2025</p>
           </footer>
      )
 }
